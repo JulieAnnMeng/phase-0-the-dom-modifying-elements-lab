0775b0a24ec7de0f22d9253992991e90fbf07885
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-07-27 19:16:58 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1'); 
+const myName = 'Julie';
+
+document.body.appendChild(newHeader); 
+newHeader.id = 'victory';
+newHeader.textContent = myName +" is the champion";
